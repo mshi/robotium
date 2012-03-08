37b2d62c9bf522663181b09538079f41ff353df9
mshi <mshi@zynga.com>
2012-03-08 16:14:11 -0500

    Added regex to getText

--- robotium-solo/src/com/jayway/android/robotium/solo/Getter.java
+++ robotium-solo/src/com/jayway/android/robotium/solo/Getter.java
@@ -1,92 +1,149 @@
 package com.jayway.android.robotium.solo;
 
 import java.util.ArrayList;
+import java.util.HashSet;
+
 import junit.framework.Assert;
 import android.app.Activity;
 import android.view.View;
 import android.widget.TextView;
 
-
 /**
- * Contains various get methods. Examples are: getView(int id),
- * getView(Class<T> classToFilterBy, int index).
+ * Contains various get methods. Examples are: getView(int id), getView(Class<T> classToFilterBy, int index).
  * 
  * @author Renas Reda, renas.reda@jayway.com
  * 
  */
 
 class Getter {
-	
-	private final ActivityUtils activityUtils;
-	private final ViewFetcher viewFetcher;
-	private final Waiter waiter;
 
-	/**
-	 * Constructs this object.
-	 * 
-	 * @param activityUtils the {@code ActivityUtil} instance.
-	 * @param viewFetcher the {@code ViewFetcher} instance.
-     * @param waiter the {@code Waiter} instance
-	 */
-	
-	public Getter(ActivityUtils activityUtils, ViewFetcher viewFetcher, Waiter waiter){
-		this.activityUtils = activityUtils;
-		this.viewFetcher = viewFetcher;
-		this.waiter = waiter;
-	}
+    private final ActivityUtils activityUtils;
+    private final ViewFetcher viewFetcher;
+    private final Waiter waiter;
+    private final int TIMEOUT = 10000;
 
-	
-	/**
-	 * Returns a {@code View} with a certain index, from the list of current {@code View}s of the specified type.
-	 *
-	 * @param classToFilterBy which {@code View}s to choose from
-	 * @param index choose among all instances of this type, e.g. {@code Button.class} or {@code EditText.class}
-	 * @return a {@code View} with a certain index, from the list of current {@code View}s of the specified type
-	 */
-	
-	public <T extends View> T getView(Class<T> classToFilterBy, int index) {
-		return waiter.waitForAndGetView(index, classToFilterBy);
-	}
-	
-	/**
-	 * Returns a {@code View} that shows a given text, from the list of current {@code View}s of the specified type.
-	 *
-	 * @param classToFilterBy which {@code View}s to choose from
-	 * @param text the text that the view shows
-	 * @param onlyVisible {@code true} if only visible texts on the screen should be returned
-	 * @return a {@code View} showing a given text, from the list of current {@code View}s of the specified type
-	 */
+    /**
+     * Constructs this object.
+     * 
+     * @param activityUtils
+     *            the {@code ActivityUtil} instance.
+     * @param viewFetcher
+     *            the {@code ViewFetcher} instance.
+     * @param waiter
+     *            the {@code Waiter} instance
+     */
 
-	public <T extends TextView> T getView(Class<T> classToFilterBy, String text, boolean onlyVisible) {
-		waiter.waitForText(text, 0, 10000, false, onlyVisible);
-		ArrayList<T> views = viewFetcher.getCurrentViews(classToFilterBy);
-		if(onlyVisible)
-			views =	RobotiumUtils.removeInvisibleViews(views);
-		T viewToReturn = null;
-		for(T view: views){
-			if(view.getText().toString().equals(text))
-				viewToReturn = view;
-		}
-		if(viewToReturn == null)
-			Assert.assertTrue("No " + classToFilterBy.getSimpleName() + " with text " + text + " is found!", false);
+    public Getter(ActivityUtils activityUtils, ViewFetcher viewFetcher, Waiter waiter) {
+        this.activityUtils = activityUtils;
+        this.viewFetcher = viewFetcher;
+        this.waiter = waiter;
+    }
 
-		return viewToReturn;
-	}
-	
-	/**
-	 * Returns a {@code View} with a given id.
-	 * 
-	 * @param id the R.id of the {@code View} to be returned
-	 * @return a {@code View} with a given id
-	 */
+    /**
+     * Returns a {@code View} with a certain index, from the list of current {@code View}s of the specified type.
+     * 
+     * @param classToFilterBy
+     *            which {@code View}s to choose from
+     * @param index
+     *            choose among all instances of this type, e.g. {@code Button.class} or {@code EditText.class}
+     * @return a {@code View} with a certain index, from the list of current {@code View}s of the specified type
+     */
 
-	public View getView(int id){
-		final Activity activity = activityUtils.getCurrentActivity(false);
+    public <T extends View> T getView(Class<T> classToFilterBy, int index) {
+        return waiter.waitForAndGetView(index, classToFilterBy);
+    }
 
-		View view = activity.findViewById(id);
-		if (view != null)
-			return view;
+    /**
+     * Returns a {@code View} that shows a given text, from the list of current {@code View}s of the specified type.
+     * 
+     * @param classToFilterBy
+     *            which {@code View}s to choose from
+     * @param text
+     *            the text that the view shows
+     * @param onlyVisible
+     *            {@code true} if only visible texts on the screen should be returned
+     * @return a {@code View} showing a given text, from the list of current {@code View}s of the specified type
+     */
 
-		return waiter.waitForView(id);
-	}
+    public <T extends TextView> T getView(Class<T> classToFilterBy, String text, boolean onlyVisible) {
+        return getView(classToFilterBy, text, onlyVisible, false, 1);
+    }
+
+    /**
+     * Returns a {@code View} that shows a given text, from the list of current {@code View}s of the specified type.
+     * 
+     * @param classToFilterBy
+     *            which {@code View}s to choose from
+     * @param text
+     *            the text that the view shows
+     * @param onlyVisible
+     *            {@code true} if only visible texts on the screen should be returned
+     * @param useRegex
+     *            {@code true} if {@value text} should be treated as regular expression
+     * @return a {@code View} showing a given text, from the list of current {@code View}s of the specified type
+     */
+
+    public <T extends TextView> T getView(Class<T> classToFilterBy, String text, boolean onlyVisible, boolean useRegex) {
+        return getView(classToFilterBy, text, onlyVisible, useRegex, 1);
+    }
+
+    /**
+     * Returns a {@code View} that shows a given text, from the list of current {@code View}s of the specified type.
+     * 
+     * @param classToFilterBy
+     *            which {@code View}s to choose from
+     * @param text
+     *            the text that the view shows
+     * @param onlyVisible
+     *            {@code true} if only visible texts on the screen should be returned
+     * @param useRegex
+     *            {@code true} if {@value text} should be treated as regular expression
+     * @param match
+     *            the regex match that should be returned (1 for first match)
+     * @return a {@code View} showing a given text, from the list of current {@code View}s of the specified type
+     */
+
+    public <T extends TextView> T getView(Class<T> classToFilterBy, String text, boolean onlyVisible, boolean useRegex, int match) {
+        waiter.waitForText(text, 0, TIMEOUT, false, onlyVisible);
+        ArrayList<T> views = viewFetcher.getCurrentViews(classToFilterBy);
+        final HashSet<TextView> uniqueTextViews = new HashSet<TextView>();
+        if (match < 1) {
+            throw new IllegalStateException("Wtf are you trying to find with less than 1 matches?");
+        }
+        if (onlyVisible)
+            views = RobotiumUtils.removeInvisibleViews(views);
+        T viewToReturn = null;
+        for (T view : views) {
+            if (useRegex && RobotiumUtils.checkAndGetMatches(text, view, uniqueTextViews) == match) {
+                viewToReturn = view;
+                break;
+            } else if (view.getText().toString().equals(text)) {
+                viewToReturn = view;
+                break;
+            }
+        }
+
+        if (viewToReturn == null) {
+            Assert.assertTrue("No " + classToFilterBy.getSimpleName() + " with text " + text + " is found!", false);
+        }
+
+        return viewToReturn;
+    }
+    /**
+     * Returns a {@code View} with a given id.
+     * 
+     * @param id
+     *            the R.id of the {@code View} to be returned
+     * @return a {@code View} with a given id
+     */
+
+    public View getView(int id) {
+        final Activity activity = activityUtils.getCurrentActivity(false);
+
+        View view = activity.findViewById(id);
+        if (view != null)
+            return view;
+
+        return waiter.waitForView(id);
+    }
 }
